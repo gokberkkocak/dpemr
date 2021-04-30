3ae1d3f36f9def600dd27bf106602a9a366d2074
Gökberk KOÇAK <gk34@st-andrews.ac.uk>
2021-04-30 17:16:19 +0100

    Tidy logger

--- src/logger.rs
+++ src/logger.rs
@@ -26,19 +26,11 @@ impl Logger {
         log_folder: Arc<String>,
     ) -> Result<(), anyhow::Error> {
         while let Some(p) = rx.recv().await {
-            Logger::worker_writer_inner(p, log_folder.clone()).await?;
+            let mut stdout_file = File::create(format!("{}/{}.out", log_folder, p.id)).await?;
+            stdout_file.write_all(p.stdout.as_bytes()).await?;
+            let mut stderr_file = File::create(format!("{}/{}.err", log_folder, p.id)).await?;
+            stderr_file.write_all(p.stderr.as_bytes()).await?;
         }
-        Ok(())
-    }
-
-    async fn worker_writer_inner(
-        p: ProcessResult,
-        log_folder: Arc<String>,
-    ) -> Result<(), anyhow::Error> {
-        let mut stdout_file = File::create(format!("{}/{}.out", log_folder, p.id)).await?;
-        stdout_file.write_all(p.stdout.as_bytes()).await?;
-        let mut stderr_file = File::create(format!("{}/{}.err", log_folder, p.id)).await?;
-        stderr_file.write_all(p.stderr.as_bytes()).await?;
         Ok(())
     }
 }
